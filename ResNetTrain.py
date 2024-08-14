f74351d7abf69b4cf0d6bd574bd6c264895c951b
Jeff Van Humbeck <62308394+JVH-YYC@users.noreply.github.com>
2024-08-14 10:19:26 -0600

    First ResNet LOO results

--- ResNetTrain.py
+++ ResNetTrain.py
@@ -12,33 +12,42 @@ Top level function for training CNN network: UVic calixarene project V2
 import ResNet.CalixNet as CNN
 import DataLoaders.CDKDataLoader as CDL
 
-pq_file_directory = 'PQFiles'
-pq_file_name = 'Alok_Thesis_Comb_10A.pq'
-csv_file_directory = 'CSVFiles'
-binding_file = 'Data excluding non-binders.csv'
-one_hot_file = 'one_hot_short.csv'
-exclude_calix = ['BH5', 'E9', 'F1', 'E2', 'E5']
-test_set = ['AP3', 'BM1']
-output_name ='AP3 BM1 Test'
-batch_size = 400
-val_split = 0.1
-training_epochs = 300
-learning_rate = 0.003
-current_iteration = 0
+initial_calix_list = ['E11', 'BH2', 'BP0', 'AH2', 'AH1',
+                      'DM1', 'BM1', 'AM2', 'AM1', 'DP2',
+                      'CP2', 'CP1', 'BP1', 'PNO2', 'PSC4',
+                      'F4', 'AP9', 'F3', 'AP8', 'F2', 'AP7',
+                      'AP6', 'E8', 'AP5', 'E7', 'AP4', 'E6',
+                      'AP3', 'E3', 'AP1', 'E1', 'DO3', 'DO2',
+                      'AO3', 'AO2', 'AO1', 'AH7', 'AH6', 'AH5']
 
-CNN.cnn_work_flow(pq_file_directory,
-                  pq_file_name,
-                  csv_file_directory,
-                  binding_file,
-                  one_hot_file,
-                  exclude_calix,
-                  test_set,
-                  output_name,
-                  batch_size,
-                  val_split,
-                  training_epochs,
-                  learning_rate,
-                  current_iteration)
+for calix in initial_calix_list:
+    pq_file_directory = 'PQFiles'
+    pq_file_name = 'Alok_Thesis_Comb_10A.pq'
+    csv_file_directory = 'CSVFiles'
+    binding_file = 'Data excluding non-binders.csv'
+    one_hot_file = 'one_hot_short.csv'
+    exclude_calix = ['BH5', 'E9', 'F1', 'E2', 'E5']
+    test_set = [calix,]
+    output_name ='Initial LOO ' + calix
+    batch_size = 400
+    val_split = 0.1
+    training_epochs = 300
+    learning_rate = 0.003
+    current_iteration = 0
+
+    CNN.cnn_work_flow(pq_file_directory,
+                    pq_file_name,
+                    csv_file_directory,
+                    binding_file,
+                    one_hot_file,
+                    exclude_calix,
+                    test_set,
+                    output_name,
+                    batch_size,
+                    val_split,
+                    training_epochs,
+                    learning_rate,
+                    current_iteration)
 
 # CNN.batch_work_flow(file_name_variable=False,
 #                     pq_file_directory='PQFiles',
