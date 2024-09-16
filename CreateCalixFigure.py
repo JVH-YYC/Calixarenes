e3a4f1e071340f517da32edfc1b5cec594e13616
Jeff Van Humbeck <62308394+JVH-YYC@users.noreply.github.com>
2024-09-16 15:16:34 -0600

    Final LOO testing on high DO CNN

--- CreateCalixFigure.py
+++ CreateCalixFigure.py
@@ -88,7 +88,7 @@ calix_setting_dict = {'A': {'color': 'blue',
                                 'size': 25}}
                             
 VCV.multi_scatter_plot(pickle_file_folder='Results Dictionaries/',
-                       list_of_pickle_files=['AttentiveFP_regression.pkl',],
+                       list_of_pickle_files=['HiDo Rd2 LOO.pkl',],
                        file_setting_dict=file_setting_dict,
                        calix_setting_dict=calix_setting_dict,
                        peptide_setting_dict=peptide_setting_dict,
@@ -96,7 +96,7 @@ VCV.multi_scatter_plot(pickle_file_folder='Results Dictionaries/',
                        plot_setting_dict=plot_setting_dict,
                        organize_by='host',
                        single_plot=False,
-                       output_name='AttentiveFP by host with systematic error',
+                       output_name='HiDo Rd2 LOO',
                        save_fig=False,
                        calculate_metrics=True,
                        group_hosts=True)
