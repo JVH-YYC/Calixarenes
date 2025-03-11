f8dcd0233c583950e813e227c7ba1710483545f2
Jeff Van Humbeck <62308394+JVH-YYC@users.noreply.github.com>
2025-03-11 15:27:38 -0600

    Final GCN regressions (LOO)

--- CreateCalixFigure.py
+++ CreateCalixFigure.py
@@ -88,7 +88,7 @@ calix_setting_dict = {'A': {'color': 'blue',
                                 'size': 25}}
                             
 VCV.multi_scatter_plot(pickle_file_folder='Results Dictionaries/',
-                       list_of_pickle_files=['RF Absolute Results DO3.pkl',],
+                       list_of_pickle_files=['Relative_GCN_regression.pkl',],
                        file_setting_dict=file_setting_dict,
                        calix_setting_dict=calix_setting_dict,
                        peptide_setting_dict=peptide_setting_dict,
@@ -96,7 +96,7 @@ VCV.multi_scatter_plot(pickle_file_folder='Results Dictionaries/',
                        plot_setting_dict=plot_setting_dict,
                        organize_by='host',
                        single_plot=False,
-                       output_name='RF Absolute Results DO3',
+                       output_name='GCN_relative',
                        save_fig=False,
                        calculate_metrics=True,
                        group_hosts=True)
